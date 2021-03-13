326d5f857a140a14566a830d9dbf15c7ccc4615a
hamzamughal <mughalh473@gmail.com>
2021-03-13 08:13:07 +0500

    moving forward

--- public/app.js
+++ public/app.js
@@ -1,15 +1,24 @@
-"use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
+import { Invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from "./classes/Payment.js";
+const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 //inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
